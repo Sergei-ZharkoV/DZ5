51c2e99dd00da352a882aafd46c30d790dce17fa
Sergei-ZharkoV <reds09@mail.ru>
2022-03-30 01:01:18 +0300

    DZ5

---
+++ less_5_1.py
@@ -0,0 +1,27 @@
+# 1. Написать генератор нечётных чисел от 1 до n (включительно),
+# используя ключевое слово yield, например:
+# >>> odd_to_15 = odd_nums(15)
+# >>> next(odd_to_15)
+# 1
+# >>> next(odd_to_15)
+# 3
+# ...
+# >>> next(odd_to_15)
+# 15
+# >>> next(odd_to_15)
+# ...StopIteration...
+
+def odd_nums_generator(numbers):
+    for num in range(1, numbers + 1, 2):
+        yield num
+
+
+odd_to = odd_nums_generator(28)
+print(*odd_to)
+
+# 2. * (вместо 1) Решить задачу генерации нечётных чисел от 1 до n (включительно),
+# не используя ключевое слово yield.
+
+answer = int(input('введите число'))
+odd_to_generator = (answer for answer in range(1, answer + 1, 2))
+print(*odd_to_generator)
